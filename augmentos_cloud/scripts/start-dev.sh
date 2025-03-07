965a9f861182f55e9a149db77fb36b58fbb13dc1
isaiahb <isaiahballah@gmail.com>
2025-03-07 23:03:36 +0000

    docker 🐳

--- augmentos_cloud/scripts/start-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ENV=development
-BASE_PORT=8000
-export ENV BASE_PORT
-
-# Start all services in development mode
-docker-compose -f docker-compose.dev.yml up -d
--- augmentos_cloud/scripts/start-prod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ENV=production
-BASE_PORT=10000
-export ENV BASE_PORT
-
-# Start all services in production mode
-docker-compose up -d
