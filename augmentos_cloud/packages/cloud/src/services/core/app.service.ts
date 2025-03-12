c1ebeb03d0f808a90b07bf23b57366a8f0398e5e
Nicolo Micheletti <michelettinik@gmail.com>
2025-03-11 20:15:42 -0700

    Add teleprompter

--- augmentos_cloud/packages/cloud/src/services/core/app.service.ts
+++ augmentos_cloud/packages/cloud/src/services/core/app.service.ts
@@ -60,6 +60,14 @@ export const LOCAL_APPS: AppI[] = [
     webhookURL: `http://${systemApps.liveTranslation.host}/webhook`,
     logoURL: `https://cloud.augmentos.org/${systemApps.liveTranslation.packageName}.png`,
     description: systemApps.liveTranslation.description,
+  },
+  {
+    packageName: systemApps.teleprompter.packageName,
+    name: systemApps.teleprompter.name,
+    tpaType: TpaType.STANDARD,
+    webhookURL: `http://${systemApps.teleprompter.host}/webhook`,
+    logoURL: `https://cloud.augmentos.org/${systemApps.teleprompter.packageName}.png`,
+    description: systemApps.teleprompter.description,
   }
 ];
 
