195b3d0cdbb35481007db154a80f646e493e1577
Roger <github@betechie.com>
2025-04-29 23:26:38 -0700

    better parsing and formatting of posthog log messages

--- augmentos_cloud/packages/utils/src/PostHogTransport.ts
+++ augmentos_cloud/packages/utils/src/PostHogTransport.ts
@@ -17,13 +17,28 @@ export class PostHogTransport extends Transport {
 	log(info: any, callback: () => void) {
 		setImmediate(() => this.emit('logged', info))  // Winston boiler-plate
 
-		const { level, message, stack, error, ...rest } = info
+		const { level, message, stack, error, service, ...rest } = info
+
+		// Extract service name from log message using regex pattern
+		const serviceMatch = typeof message === 'string' ? message.match(/\[(.+\.service)\]:? ?([\s\S]+)/) : null;
+		const serviceName = serviceMatch ? serviceMatch[1] : (service==="unknown-service" ? undefined : service);
+		const remaining_message = serviceMatch ? serviceMatch[2] : message;
 
 		if (posthog) {
 			if ((level === 'error' || level === 'warn') || (error instanceof Error || info instanceof Error)) {
-				posthog.captureException(error ?? message ?? info as Error, rest.userId || 'system', {
-					logger_level: level,
+				const error_message = `[${level}]: ${remaining_message ?? error?.message}`;
+				const error_info = {
+					level: level,
+					service: serviceName,
 					stack: stack ?? error?.stack,
+					message: error_message,
+					...rest,
+				}
+				posthog.captureException(error_info, rest.userId || 'system', {
+					level: level,
+					service: serviceName,
+					stack: stack ?? error?.stack,
+					message: error_message,
 					...rest,
 				})
 			} else {
@@ -31,11 +46,12 @@ export class PostHogTransport extends Transport {
 					distinctId: rest.userId || 'system',
 					event: `$LOG ${level}`,
 					properties: {
-						message,
+						message: remaining_message,
+						service: serviceName,
 						level,
-						...rest,
 						stack: stack ?? (error?.stack as string | undefined),
 						timestamp: Date.now(),
+						...rest,
 					},
 				})
 			}
