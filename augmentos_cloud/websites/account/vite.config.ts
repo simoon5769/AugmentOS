1df5c527ecc731c6f372c36242fa021f64cda268
Alex Israelov <alex1115alex@gmail.com>
2025-05-19 13:38:00 -0700

    try to make cf happy

--- augmentos_cloud/websites/account/vite.config.ts
+++ augmentos_cloud/websites/account/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   server: {
     port: 8052,
   },
